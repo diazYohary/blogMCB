ea437987c667ad7313306352a05a846f8a7efb20
YoharyDiazMCB <ydiaz@mcbrokers.com.mx>
2026-02-17 07:13:15 -0600

    Mejora en el componente de Dropdown menu

--- src/assets/components/DropdownMenu/DropdownMenu.jsx
+++ src/assets/components/DropdownMenu/DropdownMenu.jsx
@@ -1,5 +1,13 @@
 import React, {useState} from 'react'
+import { motion } from 'motion/react';
 import styles from './DropdownMenu.module.scss'
+
+const DownArrow = () => (
+    <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" height="12" width="12" xmlns="http://www.w3.org/2000/svg">
+        <path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>
+    </svg>
+)
+
 const DropdownMenu = ({title, options}) => {
     const [isOpen, setIsOpen]=useState(false);
 
@@ -9,17 +17,26 @@ const DropdownMenu = ({title, options}) => {
     }
     
     return (
-        <div onMouseEnter={()=>setIsOpen(true)} onMouseLeave={()=>setIsOpen(false)}>
+        <div 
+            onMouseEnter={()=>setIsOpen(true)} 
+            onMouseLeave={()=>setIsOpen(false)}
+        >
             <button className={`${styles.mcb_dropdown_trigger} mcb-flex mcb-gap-5 mcb-ai-c ${isOpen ? styles.mcb_dropdown_trigger_open:''}`} 
                 onClick={openMenu}
             >
                 {title}
-                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" height="12" width="12" xmlns="http://www.w3.org/2000/svg"><path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path></svg>    
+                <DownArrow/>
             </button>
             {isOpen && (
-                <div className={styles.mcb_dropdown_menu}>
+                <motion.div 
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ type: "spring" }}
+                    className={styles.mcb_dropdown_menu}
+                >
                     {options}
-                </div>
+                </motion.div>
             )}
 
         </div>
