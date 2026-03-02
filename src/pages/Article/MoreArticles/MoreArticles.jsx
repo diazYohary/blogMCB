de8d1a90e04a4fd93201e78f5637d68de449e553
YoharyDiazMCB <ydiaz@mcbrokers.com.mx>
2026-03-02 07:06:03 -0600

    ajustes menores en el body de un articulo y en la seccion de mas articulos

--- src/pages/Article/MoreArticles/MoreArticles.jsx
+++ src/pages/Article/MoreArticles/MoreArticles.jsx
@@ -2,8 +2,11 @@ import CardCarousel from "../../../assets/components/CardCarousel/CardCarousel";
 
 const MoreArticles=({
     articles=[]
-    
 })=>{
+
+    if(!articles.length>0){
+        return
+    }
     return(
         <section className="mcb-pd-30 mcb-flex-c mcb-gap-20">
             <h1 className="mcb-txt-c">MCBrokers recomienda</h1>
