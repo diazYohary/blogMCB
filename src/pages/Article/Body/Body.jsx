7c18c693f2f24b5b39b3d1bf8a83cef5e491b5a1
YoharyDiazMCB <ydiaz@mcbrokers.com.mx>
2026-03-04 08:03:30 -0600

    Ajustes en el loader del body del article

--- src/pages/Article/Body/Body.jsx
+++ src/pages/Article/Body/Body.jsx
@@ -1,62 +1,46 @@
 import React, { useRef } from 'react';
+import { motion } from 'motion/react';
 import styles from './Body.module.scss';
-import {postRenderer} from '../../../utils/post-renderer';
-import PDFViewer from '../../../assets/components/PDFViewer/PDFViewer';
+import { postRenderer } from '../../../utils/post-renderer';
+import Skeleton from '../../../assets/components/Skeletons/Skeleton';
+
 const Body=({
-    data:{
-        contenidos
-    }
+    data,
+    isLoading
 })=>{
     const contentRef=useRef();
 
-    // buscar todos los textos que tengan contenido.match(/^##\s.+$/gm);
-    // const sections = contenidos.reduce((acc, content, index) => {
-    //     if(content.__component !== "articulos.bloque-texto") {
-    //         return acc;
-    //     }
-    //     // array de secciones
-    //     const secciones = content.contenido.match(/^##\s.+$/gm);
-    //     if (secciones) {
-    //         secciones.forEach((seccion) => {
-    //             acc.push({
-    //                 id: `subsection-${index}`,
-    //                 title: seccion.replace('## ', ''),
-    //             });
-    //         });
-    //     }
-    //     return acc;
-    // }, []);
-
-    // const handleSection=(e)=>{
-    //     e.preventDefault();
-    //     const targetId = e.currentTarget.getAttribute('href');
-
-    //     if (contentRef.current && targetId) {
-    //         const targetElement = contentRef.current.querySelector(targetId);
-    //         if (targetElement) {
-    //             targetElement.scrollIntoView({
-    //             behavior: 'smooth',
-    //             block: 'start',
-    //             });
-    //         }
-    //     }
-    // }
+    if(isLoading) 
     return(
         <div className={`${styles.mcb_article_body} mcb-flex-c mcb-ai-c`}>
-            {/* <aside className='mcb-card mcb-gap-10 mcb-h-fc'>
-                <div>
-                    <h2 className='mcb-fs-24'>Tabla de contenido</h2>
-                    <hr />
-                </div>
-                {sections.map((i, index)=>(
-                    <a key={index} href={`#${i.title.replace(/\s+/g, '-').toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")}`} onClick={handleSection} className='mcb-w-fc'>{i.title}</a>
-                ))}
-            </aside> */}
+            <article className={`mcb-card ${styles.mcb_article_cont}`}>
+                <Skeleton height='1rem' width='100%'/>
+                <Skeleton height='1rem' width='100%'/>
+                <Skeleton height='1rem' width='100%'/>
+                <Skeleton height='1rem' width='100%'/>
+                <Skeleton height='1rem' width='100%'/>
+                <Skeleton height='1rem' width='100%'/>
+                <Skeleton height='200px' width='100%'/>
+            </article>
+        </div>
+    )
+
+    if (!data) {
+        return <p>No se encontró contenido.</p>;
+    }
+
+    const { contenidos } = data;
+    return(
+        <motion.div 
+            initial={{ opacity: 0, }}
+            animate={{ opacity: 1, }}
+            transition={{ duration: 0.2 }}
+            className={`${styles.mcb_article_body} mcb-flex-c mcb-ai-c`}
+        >
             <article ref={contentRef} className={`mcb-card ${styles.mcb_article_cont}`}>
                 {contenidos.map(postRenderer)}
             </article>
-            {/* <PDFViewer url={'https://www.uv.es/fragar/html/pdf/html11.pdf'}/> */}
-        </div>
+        </motion.div>
     )
 }
 export default Body;
